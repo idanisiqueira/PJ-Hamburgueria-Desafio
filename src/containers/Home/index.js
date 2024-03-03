17fe83d6079eebfa5df50323560ad47124e6a9a6
idanisiqueira <dcess360@gmail.com>
2024-03-03 19:36:05 -0300

    inserindo delete de orders

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // para usar a biblioteca do React e ferramentas auxiliares
+import React, { useState, useRef } from 'react'; // para usar a biblioteca do React e ferramentas auxiliares
 import axios from 'axios'; // para usar API no React
 
 import LogoOne from '../../assets/dev-burguer-logo.png'; // logotipo da primeira página
@@ -11,19 +11,17 @@ import { Container, Input, Button, Image, ContainerItens, H1, Order } from './st
 const App = () => {
 
   const [ clientOrders, setClientOrders ] = useState([]); // react hook para alterar o estado da página, fazendo assim atualizar a informação, acrescentando os valores completos na tela
-  const [ order, setOrder ] = useState(); // react hook para alterar o estado da página, fazendo atualizar a informação, acrescentando o pedido
-  const [ name, setName ] = useState(); // react hook para alterar o estado da página, fazendo atualizar a informação, acrescentando o nome
- 
+  const inputOrder = useRef(); // pegando as informações de referência no input com o react hook useRef
+  const inputName = useRef(); // pegando as informações de referência no input com o react hook useRef
+
   function addNewOrder(){ // função para adicionar o pedido com id, pedido e nome
-    setClientOrders([... clientOrders,{id: Math.random(), order, name}]) // uso de spread operator (...) fazendo acrescentar o array antigo + item novo
+    setClientOrders([... clientOrders,{id: Math.random(), order: inputOrder.current.value, name: inputName.current.value}]) // uso de spread operator (...) fazendo acrescentar o array antigo + item novo
   }
 
-  function changeInputOrder(event){ // função para captar quando o input for alterado, pegando seu valor (pedido)
-    setOrder(event.target.value)
-  }
+  function deleteOrder(orderId){ // função para deletar o pedido
+    const newClientOrder = clientOrders.filter (order => order.id !== orderId)
 
-  function changeInputName (event){ // função para captar quando o input for alterado, pegando seu valor (nome)
-    setName(event.target.value)
+    setClientOrders(newClientOrder)
   }
 
   return (
@@ -36,12 +34,12 @@ const App = () => {
 
         <div className="input-group mb-3"> 
           <span className="input-group-text" id="basic-addon1"><BsCart4 /></span>
-          <Input onChange={changeInputOrder} type="text" className="form-control" placeholder="Pedido" aria-label="Username" aria-describedby="basic-addon1" />
+          <Input ref={inputOrder} type="text" className="form-control" placeholder="Pedido" aria-label="Username" aria-describedby="basic-addon1" />
         </div>
 
         <div className="input-group mb-3">
           <span className="input-group-text" id="basic-addon1"><BsFillPersonFill /></span>
-          <Input onChange={changeInputName} type="text" className="form-control" placeholder="Nome do Cliente" aria-label="Username" aria-describedby="basic-addon1" />
+          <Input ref={inputName} type="text" className="form-control" placeholder="Nome do Cliente" aria-label="Username" aria-describedby="basic-addon1" />
         </div>
 
         <Button onClick={addNewOrder}>Novo Pedido</Button>
@@ -51,7 +49,7 @@ const App = () => {
           {clientOrders.map((order =>
             <Order key={order.id}>
               {order.order}<div></div>{order.name}
-              <button id="basic-addon2"><BsFillTrashFill /></button>
+              <button onClick = {() => deleteOrder(order.id)} id="basic-addon2"><BsFillTrashFill /></button>
             </Order>
           ))}
         </ul>
